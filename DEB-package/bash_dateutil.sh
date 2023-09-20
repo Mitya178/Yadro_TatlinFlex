2a52a0f7f08078490354ca1e3f4e3a6e29e5595d
Dmitriy Voronkov <58822703+Mitya178@users.noreply.github.com>
2023-09-20 16:57:15 +0300

    Update bash_dateutil.sh

--- DEB-package/bash_dateutil.sh
+++ DEB-package/bash_dateutil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update
-apt install dpkg-dev devscripts equivs wget python3-all, python3-setuptools, python3-six
+apt-get update
+apt install dpkg-dev devscripts equivs wget python3-all python3-setuptools python3-six
 cd python-dateutil2.8.2
 ls -la
 mk-build-deps --install
